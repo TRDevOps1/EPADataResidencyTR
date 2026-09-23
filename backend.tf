9b4eb76ddf7210f4ec3ef4c1d032978df1c5dcd3
raggsy01 <147638586+raggsy01@users.noreply.github.com>
2026-09-23 10:58:20 +0100

    changed statefile bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-aig-bo"
+    bucket       = "terraform-state-aig"
     key          = "global/s3/terraform-general.tfstate"
     region       = "eu-west-2"
     encrypt      = true
