52a922d2afe7eb4ca39c15ca7a60946c8327ccb3
raggsy01 <147638586+raggsy01@users.noreply.github.com>
2026-09-24 11:22:49 +0100

    updated region to zurich

--- modules/storage/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-aig"
-    key          = "global/poc/storage.tfstate"
-    region       = "eu-west-2"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
